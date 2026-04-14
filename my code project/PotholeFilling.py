9b05a7ecf5bfd5fb39dd6c1fd98585e264cc748e
Benny01080108 <chouyulun0108@gmail.com>
2026-04-14 17:02:41 +0800

    Update PotholeFilling.py

--- my code project/PotholeFilling.py
+++ my code project/PotholeFilling.py
@@ -2,16 +2,7 @@
 File: PotholeFilling.py
 Name: TODO:Benny
 --------------------------
-This program demonstrates how Karel fills multiple potholes
-by using decomposition.
-
-In this program, we will guide Karel to fix three potholes
-on the road. Instead of writing all the commands in one place,
-we will practice breaking a big task into smaller, reusable
-functions to make the program clearer and easier to manage.
 """
-
-
 from karel.stanfordkarel import *
 def put():
     for i in range(99):
@@ -20,23 +11,18 @@ def put():
 def right():
     for i in range(3):
         turn_left()
-
     return
 def go_out():
     for i in range(2):
         turn_left()
     move()
     right()
-
 def goin():
     for i in range(2):
         move()
     right()
     move()
     put()
-
-
-
 def main():
     move()
     right()
